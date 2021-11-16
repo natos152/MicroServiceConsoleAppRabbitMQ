b3aec4dad943636c865f64fc079fc0b900d4c5fd
Netanel Aknin <netanelos152@gmail.com>
2021-11-16 14:08:48 +0200

    Add project files.

---
+++ ReceiveList.Microservice/ReceiveData.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceiveList.Microservice
+{
+    class ReceiveData
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName, exchange: "Netanel_test", routingKey: "");
+                Console.WriteLine("waiting");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    List<Person> sJSONResponse = JsonConvert.DeserializeObject<List<Person>>(message);
+                    sJSONResponse.ForEach(i => Console.WriteLine(i));
+                };
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ SendList.Microservice/SendData.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SendList.Microservice
+{
+    class SendData
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person(DateTime.Now, "Shuki Cohen", 32, "Barber");
+            var p2 = new Person(DateTime.Now, "Dani Cohen", 35, "Barber");
+            var p3 = new Person(DateTime.Now, "Shlomi Cohen", 38, "Barber");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var list = new List<Person>() { p, p2, p3 };
+            string sJSONResponse = JsonConvert.SerializeObject(list);
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "Netanel_test", type: ExchangeType.Fanout);
+                string message = "test";
+                var body = Encoding.UTF8.GetBytes(sJSONResponse);
+                channel.BasicPublish(exchange: "Netanel_test", routingKey: "", basicProperties: null, body: body);
+                Console.WriteLine("send {0}", message);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
