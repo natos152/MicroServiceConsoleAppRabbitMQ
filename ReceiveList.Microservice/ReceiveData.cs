c9ee98e3938a451ea2d4851b63ae6cff2cff08a9
Netanel Aknin <netanelos152@gmail.com>
2021-11-16 15:09:24 +0200

    Add comments and fix some bugs on the code

--- ReceiveList.Microservice/ReceiveData.cs
+++ ReceiveList.Microservice/ReceiveData.cs
@@ -3,23 +3,28 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ReceiveList.Microservice
 {
     class ReceiveData
     {
+
         static void Main(string[] args)
         {
+            
+            //Configure connection with RabbitMQ server
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-
+                //Declare a Queue name and pull message from the sender by exchange
                 var queueName = channel.QueueDeclare().QueueName;
                 channel.QueueBind(queue: queueName, exchange: "Netanel_test", routingKey: "");
                 Console.WriteLine("waiting");
 
+                //Get the data by consumer from the binding and Deserialize the list 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
@@ -27,8 +32,11 @@ namespace ReceiveList.Microservice
                     var message = Encoding.UTF8.GetString(body);
 
                     List<Person> sJSONResponse = JsonConvert.DeserializeObject<List<Person>>(message);
+                    //Print on the console all the data from the sender
                     sJSONResponse.ForEach(i => Console.WriteLine(i));
                 };
+
+                //Start a Basic content class consumer
                 channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
                 Console.WriteLine("Press any key to exit");
--- SendList.Microservice/SendData.cs
+++ SendList.Microservice/SendData.cs
@@ -8,22 +8,29 @@ namespace SendList.Microservice
 {
     class SendData
     {
+
         static void Main(string[] args)
         {
-            var p = new Person(DateTime.Now, "Shuki Cohen", 32, "Barber");
-            var p2 = new Person(DateTime.Now, "Dani Cohen", 35, "Barber");
+            //Create a list of objects and insert them to the list
+            var p = new Person(DateTime.Now, "Shuki Cohen", 32, "Football Player");
+            var p2 = new Person(DateTime.Now, "Dani Cohen", 35, "Tennis Player");
             var p3 = new Person(DateTime.Now, "Shlomi Cohen", 38, "Barber");
+            var peopleList = new List<Person>() { p, p2, p3 };
 
+            //Covert list to JSON string to clean pass to RabbitMQ
+            string sJSONResponse = JsonConvert.SerializeObject(peopleList);
+
+            //Configure connection with RabbitMQ server
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            var list = new List<Person>() { p, p2, p3 };
-            string sJSONResponse = JsonConvert.SerializeObject(list);
 
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
+                //Declare exchange name and Fanout option to brodcast every body the channel
                 channel.ExchangeDeclare(exchange: "Netanel_test", type: ExchangeType.Fanout);
                 string message = "test";
-                var body = Encoding.UTF8.GetBytes(sJSONResponse);
+                //Show the data from Bytes to json string
+                var body = Encoding.UTF8.GetBytes(sJSONResponse); 
                 channel.BasicPublish(exchange: "Netanel_test", routingKey: "", basicProperties: null, body: body);
                 Console.WriteLine("send {0}", message);
             }
